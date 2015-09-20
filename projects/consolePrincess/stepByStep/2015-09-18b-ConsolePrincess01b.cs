4894985a11318d2b49fb4bcf562e8c3f6f221a58
Nacho Cabanes <nacho@nachocabanes.com>
2015-09-20 13:35:48 +0200

    ConsolePrincess 01b: Display text centered on screen

--- projects/consolePrincess/stepByStep/2015-09-18b-ConsolePrincess01b.cs
+++ projects/consolePrincess/stepByStep/2015-09-18b-ConsolePrincess01b.cs
@@ -3,6 +3,10 @@
    A console game by students at I.E.S. San Vicente, Spain
 */
 
+// Changes:
+// Version 0.01b : Display text centered on screen (80 cosl, 25 rows)
+// Version 0.01a : Just display some text on screen
+
 public class ConsolePrincess
 {
     public static void Main()

ff6d6377ad082b42eb542b369f8225f3f2a309a5
Nacho Cabanes <nacho@nachocabanes.com>
2015-09-20 13:32:14 +0200

    ConsolePrincess: First contact

---
+++ projects/consolePrincess/ConsolePrincess.cs
@@ -0,0 +1,12 @@
+/* 
+   Console Princess 
+   A console game by students at I.E.S. San Vicente, Spain
+*/
+
+public class ConsolePrincess
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("A");
+    }
+}
---
+++ projects/consolePrincess/stepByStep/2015-09-18a-ConsolePrincess01a.cs
@@ -0,0 +1,14 @@
+/* 
+   Console Princess 
+   A console game by students at I.E.S. San Vicente, Spain
+*/
+
+// Version 0.01a : Just display some text on screen
+
+public class ConsolePrincess
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("A");
+    }
+}

dd366dbb7d38baaf10fda4aad73320d90359b92b
Nacho Cabanes <nacho@nachocabanes.com>
2015-09-20 13:44:00 +0200

    Console Princess 01d : Read input from the user

--- projects/consolePrincess/stepByStep/2015-09-18d-ConsolePrincess01d.cs
+++ projects/consolePrincess/stepByStep/2015-09-18d-ConsolePrincess01d.cs
@@ -3,6 +3,12 @@
    A console game by students at I.E.S. San Vicente, Spain
 */
 
+// Changes:
+// Version 0.01d : Read input from the user
+// Version 0.01c : Variables, using System
+// Version 0.01b : Display text centered on screen (80 cosl, 25 rows)
+// Version 0.01a : Just display some text on screen
+
 using System;
 
 public class ConsolePrincess

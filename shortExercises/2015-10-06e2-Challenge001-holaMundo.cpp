2d0c1b9ba63cc709d5b33a459f6681723165336e
Nacho Cabanes <nacho@nachocabanes.com>
2015-10-12 23:50:10 +0200

    C++ version of the first challenge

---
+++ shortExercises/2015-10-06e2-Challenge001-holaMundo.cpp
@@ -0,0 +1,17 @@
+// "Hola mundo." n times in C++
+ 
+#include <iostream>
+using namespace std;
+
+ 
+int main() 
+{
+    int n,i;
+
+    cin >> n;
+    
+    for (i=1;i<=n;i++)
+        cout << "Hola mundo." << endl;
+        
+    return 0;
+}

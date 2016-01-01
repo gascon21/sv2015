385ebbdfcc8493bc8238b7295e5fc4c1b1dfc4ee
Nacho Cabanes <nacho@nachocabanes.com>
2016-01-02 00:45:26 +0100

    Review exercise for Dec.24

---
+++ reviews/2016-01-06a-RepeatedName.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class RepeatedName
+{
+    static void Main()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        
+        Console.Write("How many times do you want me to write it? ");
+        int times = Convert.ToInt32(Console.ReadLine());
+        
+        for (int i = 0; i < times; i++)
+        {
+            if (i % 2 == 0)
+                Console.WriteLine(name);
+            else
+                Console.WriteLine("--" + name + "--");
+        }
+    }
+}
+

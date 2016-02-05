efb1faa527d14c399a42e636e870dce9ab5be536
Nacho Cabanes <nacho@nachocabanes.com>
2016-02-05 23:21:56 +0100

    PrincessOfSanvi2 0.04a: Animated movement, first approach

--- projects/PrincessOfSanvi2/inUse/DamGame/Enemy.cs
+++ projects/PrincessOfSanvi2/inUse/DamGame/Enemy.cs
@@ -2,15 +2,23 @@
 {
     class Enemy : Sprite
     {
+        protected int stepsTillNextFrame;
+        protected int currentStep;
         public Enemy(int newX, int newY)
         {
-            LoadImage("data/bird.png");
+            LoadSequence(LEFT, 
+                new string[] { "data/birdL1.png", "data/birdL2.png"});
+            LoadSequence(RIGHT,
+                new string[] { "data/birdR1.png", "data/birdR2.png" });
+            ChangeDirection(LEFT);
             x = newX;
             y = newY;
             xSpeed = 3;
             ySpeed = 3;
             width = 48;
             height = 48;
+            stepsTillNextFrame = 5;
+            currentStep = 0;
         }
 
         public override void Move()
@@ -19,6 +27,18 @@
             if ((x > 1024 - width) || (x < 0))
                 xSpeed = -xSpeed;
             x = (short)(x + xSpeed);
+
+            if (xSpeed < 0)
+                ChangeDirection(LEFT);
+            else
+                ChangeDirection(RIGHT);
+
+            currentStep++;
+            if (currentStep >= stepsTillNextFrame)
+            {
+                currentStep = 0;
+                NextFrame();
+            }
         }
     }
 }

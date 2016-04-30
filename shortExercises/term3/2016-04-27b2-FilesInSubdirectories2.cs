37d3574313cec6235c1fc98ed9caa91922492ad9
Nacho Cabanes <nacho@nachocabanes.com>
2016-04-30 10:48:15 +0200

    Alternative version for second exercise of Apr.27

--- shortExercises/term3/2016-04-27b2-FilesInSubdirectories2.cs
+++ shortExercises/term3/2016-04-27b2-FilesInSubdirectories2.cs
@@ -1,26 +1,23 @@
 ﻿// Files in subdirectories
-// V1: GetFiles + GetDirectories
+// V2: GetFileSystemInfos
 
 using System;
 using System.IO;
 
-public class FilesInSubdirectories1
+public class FilesInSubdirectories2
 {
     public static void ShowDirectory(string name)
     {
         DirectoryInfo dir = new DirectoryInfo(name);
-        FileInfo[] file = dir.GetFiles();
+        FileSystemInfo[] file = dir.GetFileSystemInfos();
         for (int i = 0; i < file.Length; i++)
         {
             Console.WriteLine(file[i].FullName);
-        }
-
-        DirectoryInfo[] directories = dir.GetDirectories();
-        for (int i = 0; i < directories.Length; i++)
-        {
-            ShowDirectory(directories[i].FullName);
+            if ((file[i].Attributes & FileAttributes.Directory) != 0)
+                ShowDirectory(file[i].FullName);
         }
     }
+
     public static void Main()
     {
         ShowDirectory(".");

5cf16149672eb854483d50d6db27cb3a3b87f53e
Nacho Cabanes <nacho@nachocabanes.com>
2016-05-11 13:28:59 +0200

    Empty skeleton for Windows Forms version of Home Accounting

---
+++ projects/HomeAccounting/inUse/HomeAccounting2/windowsForms/MainForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace HomeAccountingWF
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.añadirTransacciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.informeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.últimasTransaccionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nocionesBásicasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.informeToolStripMenuItem,
+            this.ayudaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.añadirTransacciónToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nocionesBásicasToolStripMenuItem,
+            this.acercaDeToolStripMenuItem});
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // añadirTransacciónToolStripMenuItem
+            // 
+            this.añadirTransacciónToolStripMenuItem.Name = "añadirTransacciónToolStripMenuItem";
+            this.añadirTransacciónToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.añadirTransacciónToolStripMenuItem.Text = "Añadir transacción...";
+            this.añadirTransacciónToolStripMenuItem.Click += new System.EventHandler(this.añadirTransacciónToolStripMenuItem_Click);
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.acercaDeToolStripMenuItem.Text = "Acerca de...";
+            this.acercaDeToolStripMenuItem.Click += new System.EventHandler(this.acercaDeToolStripMenuItem_Click);
+            // 
+            // informeToolStripMenuItem
+            // 
+            this.informeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.últimasTransaccionesToolStripMenuItem});
+            this.informeToolStripMenuItem.Name = "informeToolStripMenuItem";
+            this.informeToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.informeToolStripMenuItem.Text = "Informe";
+            // 
+            // últimasTransaccionesToolStripMenuItem
+            // 
+            this.últimasTransaccionesToolStripMenuItem.Name = "últimasTransaccionesToolStripMenuItem";
+            this.últimasTransaccionesToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.últimasTransaccionesToolStripMenuItem.Text = "Últimas transacciones...";
+            this.últimasTransaccionesToolStripMenuItem.Click += new System.EventHandler(this.últimasTransaccionesToolStripMenuItem_Click);
+            // 
+            // nocionesBásicasToolStripMenuItem
+            // 
+            this.nocionesBásicasToolStripMenuItem.Name = "nocionesBásicasToolStripMenuItem";
+            this.nocionesBásicasToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.nocionesBásicasToolStripMenuItem.Text = "Nociones básicas";
+            this.nocionesBásicasToolStripMenuItem.Click += new System.EventHandler(this.nocionesBásicasToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem añadirTransacciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem informeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem últimasTransaccionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nocionesBásicasToolStripMenuItem;
+    }
+}
+
---
+++ projects/HomeAccounting/inUse/HomeAccounting2/windowsForms/MainForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeAccountingWF
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void añadirTransacciónToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TransactionViewer tv = new TransactionViewer();
+            tv.ShowDialog();
+        }
+
+        private void últimasTransaccionesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TransactionsListViewer tv = new TransactionsListViewer();
+            tv.ShowDialog();
+        }
+
+        private void nocionesBásicasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            HelpScreen h = new HelpScreen();
+            h.ShowDialog();
+        }
+
+        private void acercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CreditsScreen c = new CreditsScreen();
+            c.ShowDialog();
+        }
+    }
+}

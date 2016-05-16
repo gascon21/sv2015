e4f45e54e854287b34f68242e1199c9155e27b69
ncabanes <zilog2008@gmail.com>
2016-05-16 20:53:52 +0200

    GestorDeTareas 0.03, mejorado Tarea

--- projects/gestorDeTareas/inUse/GestorDeTareas/Tarea.cs
+++ projects/gestorDeTareas/inUse/GestorDeTareas/Tarea.cs
@@ -6,6 +6,8 @@ Version    Fecha    Por, cambios
 ---------------------------------------
  0.01   16-05-2016  Nacho, esqueleto vacío
  0.02   16-05-2016  Chen Chao y Adrian Navarro, Primera versión funcional
+ 0.03   16-05-2016  Chen Chao y Adrian Navarro, Método ToString totalmente
+                    funcional y método Contains implementado
 */
 
 using System;
@@ -15,7 +17,7 @@ class Tarea
     public DateTime Fecha { get; set; }
     public string Hora { get; set; }
     public string Descripcion { get; set; }
-    public int DuracionMin { get; set; }
+    public string DuracionMin { get; set; }
     public string Categoria { get; set; }
     public byte Prioridad { get; set; }
     public bool Completado { get; set; }
@@ -27,6 +29,9 @@ class Tarea
         this.Descripcion = descripcion;
         this.Categoria = categoria;
         this.Prioridad = prioridad;
+        this.DuracionMin = "";
+        this.Hora = "";
+        this.Completado = false;
     }
 
     public Tarea(DateTime fecha, string hora, string descripcion,
@@ -37,20 +42,12 @@ class Tarea
         this.Descripcion = descripcion;
         this.Categoria = categoria;
         this.Prioridad = prioridad;
-    }
-
-    public Tarea(DateTime fecha, string descripcion, int duracionMin,
-            string categoria, byte prioridad)
-    {
-        this.Fecha = fecha;
-        this.Descripcion = descripcion;
-        this.DuracionMin = duracionMin;
-        this.Categoria = categoria;
-        this.Prioridad = prioridad;
+        this.DuracionMin = "";
+        this.Completado = false;
     }
 
     public Tarea(DateTime fecha, string hora, string descripcion,
-            int duracionMin, string categoria, byte prioridad)
+            string duracionMin, string categoria, byte prioridad)
     {
         this.Fecha = fecha;
         this.Hora = hora;
@@ -58,29 +55,66 @@ class Tarea
         this.DuracionMin = duracionMin;
         this.Categoria = categoria;
         this.Prioridad = prioridad;
+        this.Completado = false;
     }
 
+    // Este método devuelve una cadena de texto formateado sobre la 
+    // información de la tarea, devuelve, prioridad, fecha en formato
+    // día-mes-año, la categoría y la descripción
     public override string ToString()
     {
-        //if (this.Hora == "" && this.DuracionMin == 0)
-        return "" + this.Fecha.ToString("dd-mm-yyyy") + " - " +
-            this.Categoria + " - " + this.Descripcion;
+        if (this.Categoria.Length <= 12)
+        {
+            if (this.Descripcion.Length <= 44)
+                return "" + this.Prioridad + " - " +
+                    this.Fecha.ToString("dd-mm-yyyy") + " - " +
+                    this.Categoria + new string(' ',
+                    12 - this.Categoria.Length) + " - " +
+                    this.Descripcion;
+            else
+            {
+                return "" + this.Prioridad + " - " +
+                    this.Fecha.ToString("dd-mm-yyyy") + " - " +
+                    this.Categoria + new string(' ',
+                    12 - this.Categoria.Length) + " - " +
+                    this.Descripcion.Substring(0, 44);
+            }
+        }
+        else
+        {
+            if (this.Descripcion.Length <= 44)
+                return "" + this.Prioridad + " - " +
+                    this.Fecha.ToString("dd-mm-yyyy") + " - " +
+                    this.Categoria.Substring(0, 12) + " - " +
+                    this.Descripcion;
+            else
+                return "" + this.Prioridad + " - " +
+                    this.Fecha.ToString("dd-mm-yyyy") + " - " +
+                    this.Categoria.Substring(0, 12) + " - " +
+                    this.Descripcion.Substring(0, 44);
+        }
+
     }
 
+    // Este método recibe como parámetro un texto a buscar en la
+    // descripción de la tarea sin importar mayúsculas ni minúsculas,
+    // y devuelve verdadero si se encuentra el texto, falso si no lo 
+    // encuentra.
     public bool Contains(string text)
     {
-        // TO DO
-        return true;
+        return this.Descripcion.ToLower().Contains(text.ToLower());
     }
 
+    // Este método marca una tarea como completado
     public void MarcarComoCompletada()
     {
         this.Completado = true;
     }
 
+    // Mste método recibe un numero de días a atrasar y modifica la
+    // fecha al número de días introducido
     public void Retrasar(int days)
     {
         this.Fecha.AddDays(days);
     }
 }
-

48a7bf8af2eb58cb5f97f14a4b327a315b25e0d7
Nacho Cabanes <nacho@nachocabanes.com>
2016-05-16 22:44:27 +0200

    GestorDeTareas 0.04: Primera versión de TareaRepetitiva

--- projects/gestorDeTareas/inUse/GestorDeTareas/TareaRepetitiva.cs
+++ projects/gestorDeTareas/inUse/GestorDeTareas/TareaRepetitiva.cs
@@ -1,10 +1,76 @@
 ﻿// Gestor de tareas
-// TareaRepetitiva: una tarea periódica individial
+// TareaRepetitiva: una tarea periódica individual
 
-// Version Por, cambios
-// ---------------------------------------
-//  0.01   Nacho, esqueleto vacío
+/*
+Version    Fecha    Por, cambios
+---------------------------------------
+ 0.01   16-05-2016  Nacho, esqueleto vacío
+ 0.04   16-05-2016  Indra Lopez y Pedro Perez, atributos, constructor, 
+                      metodos mostrarTarea, Posponer y Comprobar
+*/
 
+using System;
+
+enum PERIODICIDAD { A, M, S, D }; // Periodicidad: A Anual | M mensual | S Semanal | D diaria 
 class TareaRepetitiva
 {
+    public DateTime Fecha { get; set; }
+    public string Hora { get; set; }
+    public string Descripcion { get; set; }
+    public int DuracionMin { get; set; }
+    public string Categoria { get; set; }
+    public byte Prioridad { get; set; }
+    public bool Completada { get; set; }
+    public bool visible { get; set; }
+    // En esta version la periodicidad esta prefijada a anual, en el futuro 
+    // implementaremos el resto de periodicidades
+    private PERIODICIDAD periodicidad;
+
+    public TareaRepetitiva(DateTime fecha, string hora,
+        string descripcion, int duracionMin, string categoria, byte prioridad)
+    {
+        Fecha = fecha;
+        Hora = hora;
+        Descripcion = descripcion;
+        DuracionMin = duracionMin;
+        Categoria = categoria;
+        Prioridad = prioridad;
+        Completada = false;
+        visible = true;
+    }
+
+    // Si la fecha de la tarea se corresponde con la 
+    // tarea actual, la tarea seguirá siendo false ya que
+    // no se habrá realizado
+    public void Comprobar()
+    {
+        DateTime ahora = DateTime.Now;
+        if (Fecha == ahora && Completada)
+            miTarea.MarcarComoCompletada();
+    }
+
+    // La tarea una vez se pospone, pasa a no ser visible 
+    // desde la lista de tareas si la fecha de la tarea es 
+    // distinta a la actual y el año de la tarea es distinto 
+    // al año actual.
+    public void Posponer(DateTime Fecha, int days)
+    {
+        DateTime ahora = DateTime.Now;
+        DateTime anyo = new DateTime(ahora.Year);
+        if (Fecha != ahora && Fecha.Year != anyo.Year)
+        {
+            visible = false;
+            Fecha.AddDays(days);    // Añadimos los días a posponer la tarea
+        }
+    }
+
+    // Si llega el mismo día o el mismo mes
+    // que cuando pase el año, mostrar la 
+    // tarea repetitiva de nuevo en la lista de tareas.
+    public void MostrarTarea()
+    {
+        DateTime diaFuturo = DateTime.Now;
+        if (Fecha.Day == diaFuturo.Day || Fecha.Month == diaFuturo.Month)
+            visible = true;
+    }
 }

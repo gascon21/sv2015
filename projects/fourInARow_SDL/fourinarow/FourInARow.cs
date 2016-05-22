49b0f77bf7128c9a5eb2ce12d741b7759c0d5b4a
Nacho Cabanes <nacho@nachocabanes.com>
2016-05-22 12:02:01 +0200

    FourInARow SDL: Corrections in WelcomeScreen + optimized images

--- projects/fourInARow_SDL/fourinarow/FourInARow.cs
+++ projects/fourInARow_SDL/fourinarow/FourInARow.cs
@@ -1,10 +1,18 @@
-﻿//Sergio Martínez, Miguel Moya, Vicente Cuenca
+﻿/* Four in a row, SDL version
+
+
+0.01  20-May-2016  Sergio Martínez, Miguel Moya, Vicente Cuenca:
+                   First release
+0.02  21-May-2016  Nacho
+                   More standard screen dimensions.
+*/
+
 class FourInARow
 {
     static void Main()
     {
         bool fullScreen = false;
-        Hardware.Init(600, 520, 24, fullScreen);
+        Hardware.Init(800, 600, 8, fullScreen);
         bool finished = false;
 
         while (!finished)
--- projects/fourInARow_SDL/fourinarow/WelcomeScreen.cs
+++ projects/fourInARow_SDL/fourinarow/WelcomeScreen.cs
@@ -1,35 +1,62 @@
-﻿//Sergio Martínez, Miguel Moya, Vicente Cuenca
+﻿/* Four in a row, SDL version
+
+
+0.01  20-May-2016  Sergio Martínez, Miguel Moya, Vicente Cuenca:
+                   First release
+0.02  21-May-2016  Nacho
+                   Game can be quitted. Text about usable keys.
+                   Screen adjusted to 800x600
+*/
 class WelcomeScreen
 {
     public enum options { Play, Quit };
     private options optionChosen;
 
+    Image ws;
+    Image play;
+    Font f;
+
+    public WelcomeScreen()
+    {
+        ws = new Image("data/Welcome.png");
+        play = new Image("data/Play.png");
+        f = new Font("FreeSansBold.ttf", 14);
+    }
+
     public void Run()
     {
         optionChosen = options.Play;
-        Image ws = new Image("data/Welcome.png");
-        Image play = new Image("data/Play.png");
-
         bool validOptionChosen = false;
 
         Hardware.ClearScreen();
         Hardware.DrawHiddenImage(ws, 0, 0);
-        Hardware.DrawHiddenImage(play, 220, 400);
-        Hardware.ShowHiddenScreen();
+        Hardware.DrawHiddenImage(play, 120, 450);
+        Hardware.WriteHiddenText("Press P to Play, Q to Quit", 300, 550,
+            255, 255, 0, f);
         do
         {
-            if (Mouse.ColisionWith(220, 400, 460, 467, true) ||
+            if (Mouse.ColisionWith(120, 450, 360, 517, true) ||
                 Hardware.KeyPressed(Hardware.KEY_P))
             {
                 validOptionChosen = true;
                 optionChosen = options.Play;
             }
             if (Hardware.KeyPressed(Hardware.KEY_Q))
+            {
+                validOptionChosen = true;
+                optionChosen = options.Quit;
+            }
 
-                Hardware.Pause(20);
+            Hardware.Pause(20);
+
+            // Sometimes, ShowHiddenScreen does not work in the first frame,
+            // so let's redraw
+            Hardware.ShowHiddenScreen();
+
         }
         while (!validOptionChosen);
     }
+
     public options GetOptionChosen()
     {
         return optionChosen;

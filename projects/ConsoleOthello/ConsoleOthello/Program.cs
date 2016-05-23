9101423b9e20c3aa5bcdf0c15f2640f00f2b6028
ncabanes <zilog2008@gmail.com>
2016-05-23 19:26:12 +0200

    Othello 0.03: Board data are char, not string. i,j replaced by col,row. First approach to ProcessMove.

--- projects/ConsoleOthello/ConsoleOthello/Program.cs
+++ projects/ConsoleOthello/ConsoleOthello/Program.cs
@@ -1,6 +1,8 @@
 ﻿// Console Othello
 // 0.01  20-05-2016  Jose Muñoz, Monica Esteve, Carla Liarte, José Vicente Leal: First version
 // 0.02  23-05-2016  Nacho: Improved translation to English, created constructor
+// 0.03  23-05-2016  Nacho: Board data are char, not string. i,j replaced by col,row.
+//                       First approach to ProcessMove.
 
 using System;
 using System.IO;
@@ -9,43 +11,43 @@ public class Othello
 {
     const int SIZE = 8;
     static bool finished = false;
-    static string[,] table = new string[SIZE, SIZE];
+    static char[,] table = new char[SIZE, SIZE];
     // true X - negras, false O - blancas
     static bool whiteTurn = true;
 
     public Othello()
     {
-        for (int i = 0; i < SIZE; i++)
+        for (int row = 0; row < SIZE; row++)
         {
-            for (int j = 0; j < SIZE; j++)
+            for (int col = 0; col < SIZE; col++)
             {
-                table[i, j] = "-";
+                table[col, row] = '-';
             }
         }
-        table[3, 3] = "X";
-        table[3, 4] = "O";
-        table[4, 3] = "O";
-        table[4, 4] = "X";
+        table[3, 3] = 'X';
+        table[3, 4] = 'O';
+        table[4, 3] = 'O';
+        table[4, 4] = 'X';
     }
 
     public void Draw()
     {
         Console.Clear();
-        for (int i = 0; i < SIZE; i++)
+        for (int row = 0; row < SIZE; row++)
         {
-            if (i == 0)
+            if (row == 0)
             {
                 Console.Write("  ");
-                for (int j = 0; j < SIZE; j++)
+                for (int col = 0; col < SIZE; col++)
                 {
-                    Console.Write(j + 1 + " ");
+                    Console.Write(col + 1 + " ");
                 }
                 Console.WriteLine();
             }
-            Console.Write(i + 1 + " ");
-            for (int j = 0; j < SIZE; j++)
+            Console.Write(row + 1 + " ");
+            for (int col = 0; col < SIZE; col++)
             {
-                Console.Write(table[i, j] + " ");
+                Console.Write(table[col, row] + " ");
             }
             Console.WriteLine();
         }
@@ -68,16 +70,16 @@ public class Othello
         int countBlacks = 0;
         int countWhites = 0;
 
-        for (int i = 0; i < SIZE; i++)
+        for (int row = 0; row < SIZE; row++)
         {
-            for (int j = 0; j < SIZE; j++)
+            for (int col = 0; col < SIZE; col++)
             {
-                if (table[i, j] == "X")
+                if (table[col, row] == 'X')
                 {
                     countBlacks++;
                 }
 
-                if (table[i, j] == "O")
+                if (table[col, row] == 'O')
                 {
                     countWhites++;
                 }
@@ -92,31 +94,31 @@ public class Othello
         ConsoleKeyInfo key;
         do
         {
-
-            Console.Write("Enter row: ");
+            Console.Write("Enter row (1 to 8): ");
             key = Console.ReadKey();
             valid = IsInValidRange(key);
             Console.WriteLine();
         }
         while (!valid && !finished);
+
         if (!finished)
         {
             if (valid)
             {
-                x = Convert.ToInt32(key.KeyChar.ToString());
+                x = Convert.ToInt32(key.KeyChar.ToString()) - 1;
                 valid = false;
             }
 
             do
             {
-                Console.Write("Enter column: ");
+                Console.Write("Enter column (1 to 8): ");
                 key = Console.ReadKey();
                 valid = IsInValidRange(key);
                 Console.WriteLine();
             }
             while (!valid && !finished);
             if (valid && !finished)
-                y = Convert.ToInt32(key.KeyChar.ToString());
+                y = Convert.ToInt32(key.KeyChar.ToString()) - 1;
         }
     }
 
@@ -131,14 +133,36 @@ public class Othello
         return false;
     }
 
+    public void ProcessMove(int col, int row, char playerSymbol)
+    {
+        char otherPlayerSymbol = playerSymbol == 'X' ? 'O' : 'X';
+        int x, y;
 
+        // Horizontal rightwards
+        x = col; y = row;
+        // If the next cell is not ours
+        if ((col < 7) && (table[col + 1, row] == otherPlayerSymbol))
+        {
+            x ++;
+            // Skip the other player's cells
+            while ((x < 7) && (table[x, row] == otherPlayerSymbol))
+                x++;
+            // If the next one is ours, then we can turn those cells
+            if ((x < 7) && (table[x, row] == playerSymbol))
+                for (int c = col; c <= x; c++)
+                    table[c, row] = playerSymbol;
+
+        }
+
+        // TO DO: Process the other directions
+    }
 
     public void Jugada(int x, int y)
     {
         int posStart = x - 1;
         int posEnd = 8;
 
-        for (int i = x; i < posEnd; i++)
+        for (int row = x; row < posEnd; row++)
         {
 
         }
@@ -147,32 +171,32 @@ public class Othello
 
     public bool IsBoardFull()
     {
-        for (int i = 0; i < SIZE; i++)
+        for (int row = 0; row < SIZE; row++)
         {
-            for (int j = 0; j < SIZE; j++)
+            for (int col = 0; col < SIZE; col++)
             {
-                if (table[i, j] == "-")
+                if (table[col, row] == '-')
                     return false;
             }
         }
         return true;
     }
 
-    public string DisplayWinnerInfo(string[,] t)
+    public string DisplayWinnerInfo(char[,] t)
     {
         // Blancas O
         // Negras X
         int amountOfWhite = 0;
         int amountOfBlack = 0;
 
-        for (int i = 0; i < SIZE; i++)
+        for (int row = 0; row < SIZE; row++)
         {
-            for (int j = 0; j < SIZE; j++)
+            for (int col = 0; col < SIZE; col++)
             {
-                if (t[i, j] == "O")
+                if (t[col, row] == 'O')
                     amountOfWhite++;
 
-                if (t[i, j] == "X")
+                if (t[col, row] == 'X')
                     amountOfBlack++;
             }
         }
@@ -202,14 +226,16 @@ public class Othello
 
             AskPosition(ref row, ref col);
 
-            if (whiteTurn && table[row - 1, col - 1] == "-" && !finished)
+            if (whiteTurn && table[col, row] == '-' && !finished)
             {
-                table[(row - 1), (col - 1)] = "X";
+                table[col, row] = 'X';
+                ProcessMove(col, row, 'X');
                 whiteTurn = !whiteTurn;
             }
-            else if ((!whiteTurn) && table[row - 1, col - 1] == "-" && !finished)
+            else if ((!whiteTurn) && table[col, row] == '-' && !finished)
             {
-                table[(row - 1), (col - 1)] = "O";
+                table[col, row] = 'O';
+                ProcessMove(col, row, 'O');
                 whiteTurn = !whiteTurn;
             }
 
